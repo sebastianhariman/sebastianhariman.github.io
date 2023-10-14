e3655177fa2a324ea1c90174ac246c6e8e39ab58
sebastianhariman <sebastianhw11@gmail.com>
2023-10-15 00:07:39 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'), navToggle = document.getElementById('nav-toggle'), navClose = document.getElementById('nav-close')
+
+if (navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
+
+}
+
+window.addEventListener('scroll', shadowHeader)
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - sectionHeight/2,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+        
+}
+window.addEventListener('scroll', scrollActive)
